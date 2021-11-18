9e869d269de26da5caca0a2876b9f4e18769204c
heedam hwang <heedam21@gmail.com>
2021-11-18 18:59:06 +0900

    Initial Commit

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by 황희담 on 2021/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
